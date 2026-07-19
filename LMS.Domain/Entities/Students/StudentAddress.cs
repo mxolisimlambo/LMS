65bb1b0ef16c8167e6dd4f525b5ad66396fff97c
mxolisimlambo6@gmail.com <MGMlambo87!@>
2026-07-19 20:44:09 +0200

    Add StudentAddress domain entity

---
+++ LMS.Domain/Entities/Students/StudentAddress.cs
@@ -0,0 +1,28 @@
+namespace LMS.Domain.Entities.Students;
+
+public class StudentAddress
+{
+    public long StudentAddressId { get; set; }
+
+    public long StudentProfileId { get; set; }
+
+    public string AddressLine1 { get; set; } = string.Empty;
+
+    public string? AddressLine2 { get; set; }
+
+    public string City { get; set; } = string.Empty;
+
+    public string StateProvince { get; set; } = string.Empty;
+
+    public string PostalCode { get; set; } = string.Empty;
+
+    public int CountryId { get; set; }
+
+    public bool IsPrimary { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+
+    public DateTime? UpdatedDate { get; set; }
+
+    public bool IsDeleted { get; set; }
+}
